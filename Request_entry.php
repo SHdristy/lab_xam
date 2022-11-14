8c2f1be68c32fd8ee12d688d859360838d8af8e3
unknown <shassan191215@bscse.uiu.ac.bd>
2022-11-14 12:22:09 +0600

    aded new lins

---
+++ Request_entry.php
@@ -0,0 +1,32 @@
+<?php
+
+	$id = $_GET["id"];
+
+	$name = $_GET["name"];
+
+	$missed_course = $_GET["missed_course"];
+
+
+
+
+	require_once('db_connect.php');
+
+	$connect = mysqli_connect( HOST, USER, PASS, DB )
+
+		or die("Can not connect");
+
+
+
+	mysqli_query( $connect, "INSERT INTO teacher VALUES ( '', '$id', '$name' , '$missed_course' )" )
+
+		or die("Can not execute query");
+
+
+
+	echo "Record inserted:<br> id = $id <br> name = $name <br> $missed_course = missed_course";
+
+
+
+	echo "<p><a href=read.php>READ all records</a>";
+
+?>
