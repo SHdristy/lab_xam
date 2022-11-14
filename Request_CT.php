4695ce4f3ce5ebcb419a48641e975bb5b88d0410
unknown <shassan191215@bscse.uiu.ac.bd>
2022-11-14 12:20:05 +0600

    new line added for php

---
+++ Request_CT.php
@@ -0,0 +1,25 @@
+<h1>request for CT</h1>
+
+
+
+<form method=get action=Request_entry.php>
+
+	id: <input type=text name=id> <br>
+
+	<p>
+
+	name: <input type=text name= name> <br>
+
+	<p>
+
+	missed_course: <input type=text name=missed_course> <br>
+
+<p>
+ct_date: <input type=text name=ct_date> <br>
+
+<p>
+
+
+	<input type=submit value=Insert>
+
+</form>
